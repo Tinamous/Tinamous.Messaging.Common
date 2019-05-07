8fadbe920c2fa969d03c9b35baa71948fbd71701
Stephen Harrison <Stephen.Harrison@AnalysisUK.com>
2019-05-07 13:05:42 +0100

    Change common messaging main project to .Net Standard 2.0
    Changed folder to reflect project/namespace.
    
    Removed unused OctoPack packager.
    
    Updated NUnit Console runner.

--- Tinamous.Messaging.Common/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Tinamous.Messaging.Common")]
-[assembly: AssemblyCulture("")]
-[assembly: Guid("aa40e579-61a4-4f9d-8066-4981480419ff")]
