b4f7a6efb6be680adce730eefe43c1663e09de8b
Stephen Harrison <Stephen.Harrison@AnalysisUK.com>
2019-08-10 09:03:38 +0100

    Added more properties to the Process information object.

--- Tinamous.Messaging.Common/Dtos/System/ProcessInfo.cs
+++ Tinamous.Messaging.Common/Dtos/System/ProcessInfo.cs
@@ -1,4 +1,7 @@
-﻿namespace AnalysisUK.Tinamous.Messaging.Common.Dtos.System
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnalysisUK.Tinamous.Messaging.Common.Dtos.System
 {
     /// <summary>
     /// Heartbeat system process information
@@ -7,5 +10,13 @@
     {
         public long PrivateMemorySize64 { get; set; }
         public int CpuUsage { get; set; }
+        public int Threads { get; set; }
+        public TimeSpan TotalProcessorTime { get; set; }
+        public TimeSpan UserProcessorTime { get; set; }
+        public DateTime StartTime { get; set; }
+        public long VirtualMemorySize { get; set; }
+        public long WorkingSet { get; set; }
+
+        public Dictionary<string, string> More { get; set; } = new Dictionary<string, string>();
     }
 }
